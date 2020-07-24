1f8e33c9e019753d6d27fb7252d4cda6a7b467c6
Eduardo Oliveira Rodrigues <edu.ol.ro@gmail.com>
2020-07-24 11:20:08 -0300

    price_list.py update
    Foi atualizado o arquivo com novas funcionalidade. Agora além do nome e preço do produto, ele pega a descrição/informações/características dos produtos. (aula 4: https://youtu.be/pyvOmo2Uzsg)

--- examples/price_list.py
+++ examples/price_list.py
@@ -1,5 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import webbrowser
+
+
+produtos = []
+nomes = []
+precos = []
+descricao = []
 
 r = requests.get("http://testing-ground.scraping.pro/blocks?ver=1")
 if(r.status_code != 200):
@@ -14,14 +21,14 @@ else:
     for div in web_bs4.select_one("#case1").find("div").findNextSiblings():
         divs_produtos = divs_produtos + [div]
 
-    #print(divs_produtos[0]['class'])
+    #print(divs_produtos[0].select_one("span").text.replace(divs_produtos[0].select_one("span").select_one(".name").text, ''))
+
     for item in divs_produtos:
         if ("ads" in item['class']):
             divs_produtos.remove(item)
+        
 
-    produtos = []
-    nomes = []
-    precos = []
+    
     
 
     #print(div.select_one(".name").text);
@@ -29,11 +36,19 @@ else:
     for div in divs_produtos:
         nomes = nomes + [div.select_one(".name").text]
         precos = precos + [div.find("span").findNextSibling().text]
-        produtos = produtos + [(div.select_one(".name").text, div.find("span").findNextSibling().text)]
+        descricao = descricao + [div.find("span").text.replace(div.find("span").select_one(".name").text, '')]
+        #print(div.find("span").text.replace(div.find("span").select_one(".name").text, ''))
+        produtos = produtos + [(div.select_one(".name").text, div.find("span").findNextSibling().text, div.find("span").text.replace(div.find("span").select_one(".name").text, ''))]
     
-    for nome, preco in produtos:
+    for nome, preco, descricao in produtos:
         print("Nome do produto: ", nome)
         print("Preco do produto: ", preco)
-
+        print("Descricao do produto: ", descricao)
+        print("\n");
     
-#tip: use the open method to create a file and save all products to a single file!
+
+
+
+
+
+
